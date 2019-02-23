b7ad91eb43d294dcdf96d1baf35c176de054e511
Fausto Vanin <faustovanin@gmail.com>
2019-02-23 09:38:05 -0300

    Upgraded Solidity version

--- contracts/Simplify.sol
+++ contracts/Simplify.sol
@@ -1,34 +1,61 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.4;
 
-import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
-import "openzeppelin-solidity/contracts/lifecycle/Destructible.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract Simplify is Destructible, DetailedERC20, MintableToken {
-    modifier hasMintPermission() {
+contract Simplify is ERC20Detailed, ERC20Mintable {
+    modifier onlyMinter() {
         require(true);
         _;
     }
 
-	constructor (string _name, string _symbol, uint8 _decimals) public
-        DetailedERC20(_name, _symbol, _decimals)
-        Ownable()
+    mapping (address => uint256) private _flow;
+
+	constructor (string memory _name, string memory _symbol, uint8 _decimals) public
+        ERC20Detailed(_name, _symbol, _decimals)
     {
         
     }
 
-    function mint(
-        address _to,
-        uint256 _amount
-    )
+    function mint(address _to, uint256 _amount)
         public
-        hasMintPermission
-        canMint
+        onlyMinter
         returns (bool)
     {
-        return super.mint(_to, _amount);
+        bool result = super.mint(_to, _amount);
+        if(result) addFlow(_to, _amount);
+    }
+
+    function transfer(address to, uint256 value) 
+        public
+        returns (bool) 
+    {
+        bool result = super.transfer(to, value);
+        if(result) {
+            addFlow(to, value);
+            addFlow(msg.sender, value);
+        }
+    }
+
+    function transferFrom(address from, address to, uint256 value) 
+        public
+        returns (bool) 
+    {
+        bool result = super.transferFrom(from, to, value);
+        if(result) {
+            addFlow(to, value);
+            addFlow(from, value);
+        }
+    }
+
+    function addFlow(address _to, uint256 _amount) internal {
+        _flow[_to] += _amount;
+    }
+
+    function getFlow(address who) public view returns (uint256) {
+        return _flow[who];
     }
 
 }
